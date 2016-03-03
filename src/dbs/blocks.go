95898287a0cdc060c32b5dc1c7a41a33fd540586
Valentin Kuznetsov <vkuznet@gmail.com>
2016-03-02 19:44:55 -0500

    Simplify statements

--- src/dbs/blocks.go
+++ src/dbs/blocks.go
@@ -2,18 +2,18 @@ package dbs
 
 import (
 	"fmt"
-	"log"
 )
 
 // blocks API
 func blocks(params Record) []Record {
 	// query statement
-	stm := fmt.Sprintf("select id, name from blocks where name = %s", placeholder(1))
+	stm := fmt.Sprintf("select id, name from blocks where name = %s", placeholder("block_name"))
 
 	// variables we'll use in where clause
-	block := getValue(params, "block_name")
+	blocks := getValues(params, "block_name")
+	block := blocks[0]
 
 	// use generic query API to fetch the results from DB
-	out := query(stm, block)
+	out := execute(stm, block)
 	return out
 }
