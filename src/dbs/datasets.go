abf0dabdec5ff02ffc1fd4a208a765199708797f
Valentin Kuznetsov <vkuznet@gmail.com>
2016-03-02 19:47:54 -0500

    Added tokens function and provide example how to load parameters

--- src/dbs/datasets.go
+++ src/dbs/datasets.go
@@ -1,7 +1,85 @@
 package dbs
 
+import (
+	"fmt"
+	"strings"
+)
+
+// helper function to generate token's SQL statement out of given datasets
+func tokens(datasets []string) (string, []string) {
+	var vals []string
+	values := ""
+	limit := 100
+	for _, d := range datasets {
+		if values == "" { // first time
+			values = d
+			continue
+		}
+		if len(values)+1+len(d) < limit {
+			values += fmt.Sprintf(",%s", d)
+		} else {
+			vals = append(vals, values)
+			values = d
+		}
+	}
+
+	stm := ""
+	for i, _ := range vals {
+		if i > 0 {
+			stm += "\n UNION ALL \n"
+		}
+		t := fmt.Sprintf(":token%d", i)
+		stm += fmt.Sprintf("SELECT REGEXP_SUBSTR(%s, '[^,]+', 1, LEVEL) token FROM DUAL ", t)
+		stm += fmt.Sprintf("CONNECT BY LEVEL <= LENGTH(%s) - LENGTH(REPLACE(%s, ',', '')) + 1\n", t, t)
+	}
+	out := fmt.Sprintf("WITH TOKEN_GENERATOR AS(\n%s)", stm)
+	return out, vals
+}
+
+// helper function to generate operator, value pair for given argument
+func opVal(arg string) (string, string) {
+	op := "="
+	val := arg
+	if strings.Contains(arg, "*") {
+		op = "like"
+		val = strings.Replace(arg, "*", "%", -1)
+	}
+	return op, val
+}
+
 // datasets API
 func datasets(params Record) []Record {
-	var out []Record
+	// variables we'll use in where clause
+	var args []string
+	where := "WHERE "
+
+	// parse is_dataset_valid argument
+	isValid := getSingleValue(params, "is_dataset_valid")
+	if isValid == "" {
+		isValid = "0"
+	}
+	where += fmt.Sprintf("D.IS_DATASET_VALID = %s", placeholder("is_dataset_valid"))
+	args = append(args, isValid)
+
+	// parse dataset argument
+	datasets := getValues(params, "dataset")
+	genSQL := ""
+	if len(datasets) > 1 {
+		where += fmt.Sprintf(" AND D.DATASET in (SELECT TOKEN FROM TOKEN_GENERATOR)")
+		var vals []string
+		genSQL, vals = tokens(datasets)
+		for _, d := range vals {
+			args = append(args, d)
+		}
+	} else if len(datasets) == 1 {
+		op, val := opVal(datasets[0])
+		where += fmt.Sprintf(" AND D.DATASET %s %s", op, placeholder(":dataset"))
+		args = append(args, val)
+	}
+
+	// get SQL statement from static area
+	stm := getSQL("datasets")
+	// use generic query API to fetch the results from DB
+	out := execute(genSQL+stm+where, args)
 	return out
 }
