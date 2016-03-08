d4188bfaa764dd11627e6b33e04b5e4b5e0c0a00
Valentin Kuznetsov <vkuznet@gmail.com>
2016-03-08 15:22:17 -0500

    More realistics blocks API

--- src/dbs/blocks.go
+++ src/dbs/blocks.go
@@ -6,14 +6,22 @@ import (
 
 // blocks API
 func blocks(params Record) []Record {
-	// query statement
-	stm := fmt.Sprintf("select id, name from blocks where name = %s", placeholder("block_name"))
-
 	// variables we'll use in where clause
-	blocks := getValues(params, "block_name")
-	block := blocks[0]
+	var args []interface{}
+	where := "WHERE "
 
+	// parse dataset argument
+	blocks := getValues(params, "block_name")
+	if len(blocks) > 1 {
+		panic("Unsupported list of blocks")
+	} else if len(blocks) == 1 {
+		op, val := opVal(blocks[0])
+		cond := fmt.Sprintf(" B.BLOCK_NAME %s %s", op, placeholder("block_name"))
+		where += addCond(where, cond)
+		args = append(args, val)
+	}
+	// get SQL statement from static area
+	stm := getSQL("blocks")
 	// use generic query API to fetch the results from DB
-	out := execute(stm, block)
-	return out
+	return execute(stm+where, args...)
 }
