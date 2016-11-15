690c136362c65eacbf9297cd10a69dc9991e5c67
Valentin <vkuznet@gmail.com>
2016-11-15 21:44:58 +0100

    New BlockOrigin API

--- dbs/blocks.go
+++ dbs/blocks.go
@@ -99,3 +99,34 @@ func (API) BlockSummaries(params Record) []Record {
 	// use generic query API to fetch the results from DB
 	return executeAll(genSQL+stm, args...)
 }
+
+// blockorigin API
+func (API) BlockOrigin(params Record) []Record {
+	// variables we'll use in where clause
+	var args []interface{}
+	where := "WHERE "
+
+	// parse dataset argument
+	block := getValues(params, "block_name")
+	if len(block) > 1 {
+		panic("Unsupported list of block")
+	} else if len(block) == 1 {
+		op, val := opVal(block[0])
+		cond := fmt.Sprintf(" B.BLOCK_NAME %s %s", op, placeholder("block_name"))
+		where += addCond(where, cond)
+		args = append(args, val)
+	}
+	dataset := getValues(params, "dataset")
+	if len(dataset) > 1 {
+		panic("Unsupported list of dataset")
+	} else if len(dataset) == 1 {
+		op, val := opVal(dataset[0])
+		cond := fmt.Sprintf(" DS.DATASET %s %s", op, placeholder("dataset"))
+		where += addCond(where, cond)
+		args = append(args, val)
+	}
+	// get SQL statement from static area
+	stm := getSQL("blockorigin")
+	// use generic query API to fetch the results from DB
+	return executeAll(stm+where, args...)
+}
