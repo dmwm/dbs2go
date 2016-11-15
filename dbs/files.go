7fb80e8a6f552d82759c394ca1d90ebc4ce1aa39
Valentin <vkuznet@gmail.com>
2016-11-16 00:53:55 +0100

    New FileChildren API

--- dbs/files.go
+++ dbs/files.go
@@ -44,6 +44,28 @@ func (API) Files(params Record) []Record {
 	return executeAll(stm+where, args...)
 }
 
+// filechildren API
+func (API) FileChildren(params Record) []Record {
+	// variables we'll use in where clause
+	var args []interface{}
+	where := "WHERE "
+
+	// parse dataset argument
+	filechildren := getValues(params, "logical_file_name")
+	if len(filechildren) > 1 {
+		panic("The filechildren API does not support list of filechildren")
+	} else if len(filechildren) == 1 {
+		op, val := opVal(filechildren[0])
+		cond := fmt.Sprintf(" F.LOGICAL_FILE_NAME %s %s", op, placeholder("logical_file_name"))
+		where += addCond(where, cond)
+		args = append(args, val)
+	}
+	// get SQL statement from static area
+	stm := getSQL("filechildren")
+	// use generic query API to fetch the results from DB
+	return executeAll(stm+where, args...)
+}
+
 // fileparent API
 func (API) FileParent(params Record) []Record {
 	// variables we'll use in where clause
