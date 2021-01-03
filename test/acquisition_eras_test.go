3b24ccdbe4f8ed6c071dfa6d41b77baca01bbadd
Valentin Kuznetsov <vkuznet@gmail.com>
2021-01-03 12:51:19 -0500

    New files for AE table

---
+++ test/acquisition_eras_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/vkuznet/dbs2go/dbs"
+	"github.com/vkuznet/dbs2go/utils"
+)
+
+// TestAcquisitionEras API
+func TestAcquisitionEras(t *testing.T) {
+
+	// initialize DB for testing
+	db := initDB()
+	defer db.Close()
+
+	// prepare record for insertion
+	rec := make(dbs.Record)
+	rec["acquisition_era_id"] = 0
+	rec["acquisition_era_name"] = "era"
+	rec["start_date"] = 123
+	rec["end_date"] = 321
+	rec["creation_date"] = 123
+	rec["create_by"] = "tester"
+	rec["description"] = "description"
+
+	// insert new record
+	var api dbs.API
+	utils.VERBOSE = 1
+	err := api.InsertAcquisitionEras(rec)
+	if err != nil {
+		t.Errorf("Fail in insert record %+v, error %v\n", rec, err)
+	}
+
+	// fetch this record from DB, here we can either use nil writer
+	// or use StdoutWriter instance (defined in test/main.go)
+	params := make(dbs.Record)
+	var w http.ResponseWriter
+	w = StdoutWriter("")
+	log.Println("Fetch data from AcquisitionEras API")
+	_, err = api.AcquisitionEras(params, w)
+	if err != nil {
+		t.Errorf("Fail to look-up data tiers %v\n", err)
+	}
+}
