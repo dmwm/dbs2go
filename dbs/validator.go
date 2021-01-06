8f5e00fbce6c0cc3a3c8209d4d648b2fad04106a
Valentin Kuznetsov <vkuznet@gmail.com>
2021-01-06 08:37:23 -0500

    First draft implementation for validator

--- dbs/validator.go
+++ dbs/validator.go
@@ -1,8 +1,39 @@
 package dbs
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"golang.org/x/exp/errors"
+)
 
 // Validate provides validation of all input parameters of HTTP request
 func Validate(r *http.Request) error {
+	if r.Method == "GET" {
+		for k, v := range r.URL.Query() {
+			log.Printf("query parameter key=%s values=%+v\n", k, v)
+		}
+	} else if r.Method == "POST" {
+		var rec Record
+		var unmarshalErr *json.UnmarshalTypeError
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&rec)
+		if err != nil {
+			if errors.As(err, &unmarshalErr) {
+				return errors.New("Bad Request. Wrong Type provided for field " + unmarshalErr.Field)
+			} else {
+				return errors.New("Bad Request " + err.Error())
+			}
+			return err
+		}
+		// validate post form
+		return validatePostRequest(rec)
+	}
+	return nil
+}
+
+// helper function to validate POST request
+func validatePostRequest(rec Record) error {
 	return nil
 }
