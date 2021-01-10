c55915379a3583aa34822b9c8c0799724668bd01
Valentin Kuznetsov <vkuznet@gmail.com>
2021-01-10 13:50:13 -0500

    New template

---
+++ static/sql/files_sumoverlumi.sql
@@ -0,0 +1,28 @@
+with myfiles as (
+    {{.Statement}}
+) select mf.* ,
+            (case
+                when badi.file_id = mc.file_id and badi.run_num=mc.run_num and badi.bid is null then null
+                else  mc.event_count
+             end) as event_count
+     from myfiles mf,
+{{if .LumiList}}
+{{.LumiGenerator}}
+(select sum(fl.event_count) as event_count, fl.file_id, fl.run_num
+                            from {{.Owner}}.file_lumis fl
+                            join myfiles mf on mf.file_id=fl.file_id and mf.run_num=fl.run_num
+                            where sql_lumi
+                            group by fl.file_id, fl.run_num) mc
+{{else}}
+(select sum(fl.event_count) as event_count, fl.file_id, fl.run_num
+                            from {{.Owner}}.file_lumis fl
+                            join myfiles mf on mf.file_id=fl.file_id and mf.run_num=fl.run_num
+                            group by fl.file_id, fl.run_num) mc
+{{end if}}
+          (
+            select distinct fl.file_id, fl.run_num, null as bid
+            from {{.Owner}}.file_lumis fl
+            join myfiles my2 on my2.file_id=fl.file_id and my2.run_num=fl.run_num
+            where fl.event_count is null
+         )badi
+where mf.file_id= mc.file_id and mf.run_num=mc.run_num
