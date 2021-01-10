79e17f328199e0a8b21cb138337ac6b846574fda
Valentin Kuznetsov <vkuznet@gmail.com>
2021-01-10 11:18:11 -0500

    Add fielparentbylumis API

--- dbs/fileparentbylumis.go
+++ dbs/fileparentbylumis.go
@@ -1,5 +1,52 @@
 package dbs
 
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+// FileParentByLumis DBS API
+func (API) FileParentByLumis(params Record, w http.ResponseWriter) (int64, error) {
+	var args []interface{}
+	var conds []string
+
+	tmpl := make(Record)
+	tmpl["ChildLfnList"] = false
+
+	childBlockName := getValues(params, "child_block_name")
+	if len(childBlockName) == 0 {
+		return 0, errors.New("Missing child block_name for listFileParentsByLumi")
+	}
+
+	childLfnList := getValues(params, "child_lfn_list")
+	if len(childLfnList) > 1 {
+		tmpl["ChildLfnList"] = true
+		token, binds := TokenGenerator(childLfnList, 100) // 100 is max for # of allowed entries
+		tmpl["LfnTokenGenerator"] = token
+		for _, v := range binds {
+			args = append(args, v)
+		}
+	}
+
+	// get SQL statement from static area
+	stm := LoadTemplateSQL("fileparentbylumis", tmpl)
+	stm = WhereClause(stm, conds)
+
+	// fix binding variables
+	for k, v := range params {
+		key := fmt.Sprintf(":%s", strings.ToLower(k))
+		if strings.Contains(stm, key) {
+			stm = strings.Replace(stm, key, "?", -1)
+			args = append(args, v)
+		}
+	}
+
+	// use generic query API to fetch the results from DB
+	return executeAll(w, stm, args...)
+}
+
 // InsertFileParentByLumis DBS API
 func (API) InsertFileParentByLumis(values Record) error {
 	return InsertValues("insert_file_parent_by_lumis", values)
---
+++ static/sql/fileparentbylumis.sql
@@ -0,0 +1,44 @@
+{{if .ChildLfnList }}
+
+{{.LfnTokenGenerator}}
+with parents as (
+select run_num as R, Lumi_section_num as L, file_id as pid from {{.Owner}}.file_lumis fl
+where fl.file_id in (
+	select file_id from {{.Owner}}.files f
+	where F.DATASET_ID in (
+		select parent_dataset_id from {{.Owner}}.dataset_parents dp
+		inner join {{.Owner}}.datasets d on d.dataset_id=DP.THIS_DATASET_ID
+		where d.dataset = :child_ds_name )
+)
+),
+children as (
+select  run_num as R, Lumi_section_num as L, file_id as cid from {{.Owner}}.file_lumis fl
+where fl.file_id in (
+	select file_id from {{.Owner}}.files f
+	inner join {{.Owner}}.blocks b on f.block_id = b.block_id
+	where b.block_name = :child_block_name 
+		and f.logical_file_name in (SELECT TOKEN FROM TOKEN_GENERATOR) )
+)
+select distinct cid, pid from children c inner join parents p on c.R = p.R and c.L = p.L
+
+{{else}}
+
+with parents as (
+select run_num as R, Lumi_section_num as L, file_id as pid from {{.Owner}}.file_lumis fl
+where fl.file_id in (
+	select file_id from {{.Owner}}.files f
+	where F.DATASET_ID in (
+		select parent_dataset_id from {{.Owner}}.dataset_parents dp
+		inner join {{.Owner}}.datasets d on d.dataset_id=DP.THIS_DATASET_ID
+		where d.dataset = :child_ds_name )
+)
+),
+children as (
+select  run_num as R, Lumi_section_num as L, file_id as cid from {{.Owner}}.file_lumis fl
+where fl.file_id in (
+	select file_id from {{.Owner}}.files f
+	inner join {{.Owner}}.blocks b on f.block_id = b.block_id
+	where b.block_name = :child_block_name )
+)
+select distinct cid, pid from children c inner join parents p on c.R = p.R and c.L = p.L
+{{end if}}
