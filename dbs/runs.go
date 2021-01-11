e9bc44cbb2d2fa375345b7fecb51cd8fc97de5df
Valentin Kuznetsov <vkuznet@gmail.com>
2021-01-11 10:08:32 -0500

    move SQL joins from code to template

--- dbs/runs.go
+++ dbs/runs.go
@@ -11,32 +11,32 @@ func (API) Runs(params Record, w http.ResponseWriter) (int64, error) {
 	var args []interface{}
 	var conds []string
 
-	// parse dataset argument
+	tmpl := make(Record)
 	runs := getValues(params, "run_num")
 	lfn := getValues(params, "logical_file_name")
 	block := getValues(params, "block_name")
 	dataset := getValues(params, "dataset")
 
-	stm := getSQL("runs")
 	if len(runs) > 1 {
 		msg := "The runs API does not support list of runs"
 		return 0, errors.New(msg)
 	} else if len(runs) == 1 {
 		conds, args = AddParam("run_num", "FL.run_num", params, conds, args)
 	} else if len(lfn) == 1 {
-		stm += fmt.Sprintf(" inner join %s.FILES FILES on FILES.FILE_ID = FL.FILE_ID", DBOWNER)
+		tmpl["Lfn"] = true
 		conds, args = AddParam("logical_file_name", "FILES.LOGICAL_FILE_NAME", params, conds, args)
 	} else if len(block) == 1 {
-		stm += fmt.Sprintf(" inner join %s.FILES FILES on FILES.FILE_ID = FL.FILE_ID inner join %s.BLOCKS BLOCKS on BLOCKS.BLOCK_ID = FILES.BLOCK_ID", DBOWNER, DBOWNER)
+		tmpl["Block"] = true
 		conds, args = AddParam("block_name", "BLOCKS.BLOCK_NAME", params, conds, args)
 	} else if len(dataset) == 1 {
-		stm += fmt.Sprintf(" inner join %s.FILES FILES on FILES.FILE_ID = FL.FILE_ID inner join %s.DATASETS DATASETS on DATASETS.DATASET_ID = FILES.DATASET_ID", DBOWNER, DBOWNER)
+		tmpl["Dataset"] = true
 		conds, args = AddParam("dataset", "DATASETS.DATASET", params, conds, args)
 	} else {
 		msg := fmt.Sprintf("No arguments for runs API")
 		return 0, errors.New(msg)
 	}
 
+	stm := LoadTemplateSQL("runs", tmpl)
 	stm = WhereClause(stm, conds)
 
 	// use generic query API to fetch the results from DB
--- static/sql/runs.sql
+++ static/sql/runs.sql
@@ -1,1 +1,12 @@
 SELECT DISTINCT FL.RUN_NUM FROM {{.Owner}}.FILE_LUMIS FL
+{{if .Lfn}}
+inner join {{.Owner}}.FILES FILES on FILES.FILE_ID = FL.FILE_ID
+{{end}}
+{{if .Block}}
+inner join {{.Owner}}.FILES FILES on FILES.FILE_ID = FL.FILE_ID
+inner join {{.Owner}}.BLOCKS BLOCKS on BLOCKS.BLOCK_ID = FILES.BLOCK_ID
+{{end}}
+{{if .Dataset}}
+inner join {{.Owner}}.FILES FILES on FILES.FILE_ID = FL.FILE_ID
+inner join {{.Owner}}.DATASETS DATASETS on DATASETS.DATASET_ID = FILES.DATASET_ID
+{{end}}
