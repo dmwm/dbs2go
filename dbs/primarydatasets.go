8d870348b0f550fe01b4bd54bcb304c1109cc686
Valentin Kuznetsov <vkuznet@gmail.com>
2021-03-01 14:11:32 -0500

    Add logic of insert API

--- dbs/primarydatasets.go
+++ dbs/primarydatasets.go
@@ -1,6 +1,8 @@
 package dbs
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 )
 
@@ -20,12 +22,47 @@ func (API) PrimaryDatasets(params Record, w http.ResponseWriter) (int64, error) 
 }
 
 // InsertPrimaryDatasets DBS API
-func (API) InsertPrimaryDatasets(values Record) error {
-	// TODO: implement the following logic
+func (api API) InsertPrimaryDatasets(values Record) error {
+	// implement the following logic
 	// /Users/vk/CMS/DMWM/GIT/DBS/Server/Python/src/dbs/business/DBSPrimaryDataset.py
 	// intput values: primary_ds_name, primary_ds_type, creation_date, create_by
 	// insert primary_ds_type and get primary_ds_type_id
 	// businput["primary_ds_id"] = self.sm.increment(conn, "SEQ_PDS")
 	// insert primary_ds_name, creation_date, create_by, primary_ds_id
-	return InsertValues("insert_primary_datasets", values)
+
+	params := []string{"primary_ds_name", "primary_ds_type", "creation_date", "create_by"}
+	if !checkParams(values, params) {
+		msg := fmt.Sprintf("Not sufficient number of parameters %s, we expect %s", values, params)
+		return errors.New(msg)
+	}
+
+	// start transaction
+	tx, err := DB.Begin()
+	if err != nil {
+		msg := fmt.Sprintf("unable to get DB transaction %v", err)
+		return errors.New(msg)
+	}
+	defer tx.Rollback()
+
+	// if primary_ds_id is not given we will insert primary ds type first
+	if _, ok := values["primary_ds_id"]; !ok {
+		rec := make(Record)
+		rec["primary_ds_type"] = values["primary_ds_type"]
+		api.InsertPrimaryDSTypes(values)
+		pid, err := LastInsertId(tx, "primary_ds_types", "primary_ds_id")
+		if err != nil {
+			return err
+		}
+		values["primary_ds_id"] = pid
+	}
+	delete(values, "primary_ds_type")
+	res := InsertValues("insert_primary_datasets", values)
+
+	// commit transaction
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	return res
 }
