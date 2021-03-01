dc2188ccd234f12170b72cae99dbf899fea18839
Valentin Kuznetsov <vkuznet@gmail.com>
2021-03-01 13:20:29 -0500

    Add unit test for validatepostpayload function

--- test/validator_test.go
+++ test/validator_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"testing"
 
@@ -32,6 +34,25 @@ func TestValidator(t *testing.T) {
 	}
 	rurl = host + "/datasets?dataset=/*/*/*"
 	req, _ = http.NewRequest("GET", rurl, nil)
+	err = dbs.Validate(req)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+// TestValidatePostPayload
+func TestValidatePostPayload(t *testing.T) {
+	var req *http.Request
+	var err error
+	host := "http://localhost:8111/dbs2go"
+	rurl := host + "/datatiers?data_tier_name=*"
+	rec := make(map[string]string)
+	rec["data_tier_name"] = "RAW"
+	data, err := json.Marshal(rec)
+	if err != nil {
+		t.Error(err)
+	}
+	req, _ = http.NewRequest("POST", rurl, bytes.NewBuffer(data))
 	err = dbs.Validate(req)
 	if err != nil {
 		t.Error(err)
