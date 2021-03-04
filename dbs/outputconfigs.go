b28de29f2fed8ac912277927045d486839478829
Valentin Kuznetsov <vkuznet@gmail.com>
2021-03-04 09:09:20 -0500

    Implement OutputConfigs DBRecord interface

--- dbs/outputconfigs.go
+++ dbs/outputconfigs.go
@@ -1,10 +1,17 @@
 package dbs
 
 import (
+	"database/sql"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"unsafe"
+
+	"github.com/vkuznet/dbs2go/utils"
 )
 
 // OutputConfigs DBS API
@@ -71,6 +78,12 @@ func (API) InsertOutputConfigs(values Record) error {
 		return err
 	}
 	if DBOWNER == "sqlite" {
+
+		// TODO:
+		// b = read data from reader
+		// create reader = bytes.NewReader(b)
+		// pass new reader to decode method of different DBRecords
+
 		// we decouple multiple inserts into individual one
 
 		// start transaction
@@ -142,4 +155,95 @@ func (API) InsertOutputConfigs(values Record) error {
 		return err
 	}
 	return InsertValues("insert_outputconfigs", values)
+}
+
+// OutputConfigs
+type OutputConfigs struct {
+	OUTPUT_MOD_CONFIG_ID  int64  `json:"output_mod_config_id"`
+	APP_EXEC_ID           int64  `json:"app_exec_id"`
+	RELEASE_VERSION_ID    int64  `json:"release_version_id"`
+	PARAMETER_SET_HASH_ID int64  `json:"parameter_set_hash_id"`
+	OUTPUT_MODULE_LABEL   string `json:"output_module_label"`
+	GLOBAL_TAG            string `json:"global_tag"`
+	SCENARIO              string `json:"scenario"`
+	CREATION_DATE         int64  `json:"creation_date"`
+	CREATE_BY             string `json:"create_by"`
+}
+
+// Insert implementation of OutputConfigs
+func (r *OutputConfigs) Insert(tx *sql.Tx) error {
+	var tid int64
+	var err error
+	if r.OUTPUT_MOD_CONFIG_ID == 0 {
+		if DBOWNER == "sqlite" {
+			tid, err = LastInsertId(tx, "OUTPUT_MODULE_CONFIGS", "output_mod_config_id")
+			r.OUTPUT_MOD_CONFIG_ID = tid + 1
+		} else {
+			tid, err = IncrementSequence(tx, "SEQ_OMC")
+			r.OUTPUT_MOD_CONFIG_ID = tid
+		}
+		if err != nil {
+			return err
+		}
+	}
+	// get SQL statement from static area
+	stm := getSQL("insert_tiers")
+	if DBOWNER == "sqlite" {
+		stm = getSQL("insert_tiers_sqlite")
+	}
+	if utils.VERBOSE > 0 {
+		log.Printf("Insert OutputConfigs\n%s\n%+v", stm, r)
+	}
+	_, err = tx.Exec(stm, r.OUTPUT_MOD_CONFIG_ID, r.APP_EXEC_ID, r.RELEASE_VERSION_ID, r.PARAMETER_SET_HASH_ID, r.GLOBAL_TAG, r.SCENARIO, r.CREATION_DATE, r.CREATE_BY)
+	return err
+}
+
+// Validate implementation of OutputConfigs
+func (r *OutputConfigs) Validate() error {
+	if matched := unixTimePattern.MatchString(fmt.Sprintf("%d", r.CREATION_DATE)); !matched {
+		return errors.New("invalid pattern for createion date")
+	}
+	if r.OUTPUT_MODULE_LABEL == "" {
+		return errors.New("missing data_output_module_label")
+	}
+	if r.CREATION_DATE == 0 {
+		return errors.New("missing creation_date")
+	}
+	if r.CREATE_BY == "" {
+		return errors.New("missing create_by")
+	}
+	return nil
+}
+
+// Decode implementation for OutputConfigs
+func (r *OutputConfigs) Decode(reader io.Reader) (int64, error) {
+	// init record with given data record
+	data, err := ioutil.ReadAll(reader)
+	if err != nil {
+		log.Println("fail to read data", err)
+		return 0, err
+	}
+	err = json.Unmarshal(data, &r)
+
+	//     decoder := json.NewDecoder(r)
+	//     err := decoder.Decode(&rec)
+	if err != nil {
+		log.Println("fail to decode data", err)
+		return 0, err
+	}
+	size := int64(len(data))
+	return size, nil
+}
+
+// Size implementation for OutputConfigs
+func (r *OutputConfigs) Size() int64 {
+	size := int64(unsafe.Sizeof(*r))
+	size += int64(len(r.OUTPUT_MODULE_LABEL))
+	size += int64(len(r.CREATE_BY))
+	return size
+}
+
+// PostOutputConfigs DBS API
+func (API) PostOutputConfigs(r io.Reader) (int64, error) {
+	return insertRecord(&OutputConfigs{}, r)
 }
