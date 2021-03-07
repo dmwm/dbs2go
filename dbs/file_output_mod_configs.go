c760b0d6e3f7c78922bf4a0059f65f34b87e3fa7
Valentin Kuznetsov <vkuznet@gmail.com>
2021-03-07 12:52:33 -0500

    implement logic of InsertFileOutputModConfigs

--- dbs/file_output_mod_configs.go
+++ dbs/file_output_mod_configs.go
@@ -3,6 +3,8 @@ package dbs
 import (
 	"database/sql"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -82,7 +84,88 @@ func (r *FileOutputModConfigs) Decode(reader io.Reader) (int64, error) {
 	return size, nil
 }
 
+// FileOutputModConfigRecord
+type FileOutputModConfigRecord struct {
+	ReleaseVersion    string `json:"release_version"`
+	PsetHash          string `json:"pset_hash"`
+	Lfn               string `json:"lfn"`
+	AppName           string `json:"app_name"`
+	OutputModuleLabel string `json:"output_module_label"`
+	GlobalTag         string `json:"global_tag"`
+}
+
 // InsertFileOutputModConfigs DBS API
 func (API) InsertFileOutputModConfigs(r io.Reader) (int64, error) {
-	return insertRecord(&FileOutputModConfigs{}, r)
+	//     return insertRecord(&FileOutputModConfigs{}, r)
+	// read given input
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Println("fail to read data", err)
+		return 0, err
+	}
+	size := int64(len(data))
+	var rec FileOutputModConfigRecord
+	err = json.Unmarshal(data, &rec)
+	if err != nil {
+		log.Println("fail to decode data", err)
+		return 0, err
+	}
+
+	// start transaction
+	tx, err := DB.Begin()
+	if err != nil {
+		msg := fmt.Sprintf("unable to get DB transaction %v", err)
+		return 0, errors.New(msg)
+	}
+	defer tx.Rollback()
+
+	fid, err := getTxtID(tx, "FILES", "logical_file_id", "logical_file_name", rec.Lfn)
+	if err != nil {
+		log.Println("unable to find file_id for", rec.Lfn)
+		return 0, err
+	}
+	// find output module config id
+	var args []interface{}
+	var conds []string
+	params := make(Record)
+	params["logical_file_name"] = rec.Lfn
+	params["app_name"] = rec.AppName
+	params["pset_hash"] = rec.PsetHash
+	params["output_module_label"] = rec.OutputModuleLabel
+	params["global_tag"] = rec.GlobalTag
+	conds, args = AddParam("logical_file_name", "FS.LOGICAL_FILE_NAME", params, conds, args)
+	conds, args = AddParam("app_name", "A.APP_NAME", params, conds, args)
+	conds, args = AddParam("pset_hash", "P.PSET_HASH", params, conds, args)
+	conds, args = AddParam("output_module_label", "O.OUTPUT_MODULE_LABEL", params, conds, args)
+	conds, args = AddParam("global_tag", "P.GLOBAL_TAG", params, conds, args)
+	tmpl := make(Record)
+	tmpl["Owner"] = DBOWNER
+	stm, err := LoadTemplateSQL("outputconfigs", tmpl)
+	if err != nil {
+		return 0, err
+	}
+	stm = WhereClause(stm, conds)
+	var oid int64
+	err = tx.QueryRow(stm, args...).Scan(&oid)
+	if err != nil {
+		log.Printf("unable to find output_mod_config_id for %+v", rec)
+		return 0, err
+	}
+
+	// init all foreign Id's in output config record
+	var rrr FileOutputModConfigs
+	rrr.FILE_ID = fid
+	rrr.OUTPUT_MOD_CONFIG_ID = oid
+	err = rrr.Insert(tx)
+	if err != nil {
+		return 0, err
+	}
+
+	// commit transaction
+	err = tx.Commit()
+	if err != nil {
+		log.Println("faile to insert_outputconfigs_sqlite", err)
+		return 0, err
+	}
+	return size, err
 }
