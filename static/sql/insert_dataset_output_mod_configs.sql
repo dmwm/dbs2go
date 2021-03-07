da1db211964d842452791d1088c54886609902eb
Valentin Kuznetsov <vkuznet@gmail.com>
2021-03-07 11:18:04 -0500

    new templates

---
+++ static/sql/insert_dataset_output_mod_configs.sql
@@ -0,0 +1,4 @@
+INSERT INTO {{.DBOwner}}.DATASET_OUTPUT_MOD_CONFIGS
+    (ds_output_config_id,dataset_id,output_mod_config_id)
+    VALUES
+    (:ds_output_config_id,:dataset_id,:output_mod_config_id)
---
+++ static/sql/insert_dataset_output_mod_configs_sqlite.sql
@@ -0,0 +1,4 @@
+INSERT INTO DATASET_OUTPUT_MOD_CONFIGS
+    (ds_output_config_id,dataset_id,output_mod_config_id)
+    VALUES
+    (?,?,?)
