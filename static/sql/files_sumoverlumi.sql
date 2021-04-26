8b2d149d0077bc0c87ce3eb6190832a1b2a3215e
Valentin Kuznetsov <vkuznet@gmail.com>
2021-04-26 13:49:18 -0400

    Add missing comma

--- static/sql/files_sumoverlumi.sql
+++ static/sql/files_sumoverlumi.sql
@@ -12,12 +12,12 @@ with myfiles as (
                             from {{.Owner}}.file_lumis fl
                             join myfiles mf on mf.file_id=fl.file_id and mf.run_num=fl.run_num
                             where sql_lumi
-                            group by fl.file_id, fl.run_num) mc
+                            group by fl.file_id, fl.run_num) mc,
 {{else}}
 (select sum(fl.event_count) as event_count, fl.file_id, fl.run_num
                             from {{.Owner}}.file_lumis fl
                             join myfiles mf on mf.file_id=fl.file_id and mf.run_num=fl.run_num
-                            group by fl.file_id, fl.run_num) mc
+                            group by fl.file_id, fl.run_num) mc,
 {{end}}
           (
             select distinct fl.file_id, fl.run_num, null as bid
