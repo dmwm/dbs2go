3f2006bc8dad137e0f5fa35718982951e4814944
Valentin Kuznetsov <vkuznet@gmail.com>
2021-04-28 19:10:12 -0400

    Accommodate list of blocks as input

--- dbs/blocksummaries.go
+++ dbs/blocksummaries.go
@@ -21,12 +21,25 @@ func (API) BlockSummaries(params Record, w http.ResponseWriter) (int64, error) {
 	block := getValues(params, "block_name")
 	genSQL := ""
 	if len(block) > 0 {
-		if strings.Contains(block[0], "*") {
+		blk := block[0]
+		if strings.Contains(blk, "*") {
 			msg := "wild-card block value is not allowed"
 			return 0, errors.New(msg)
 		}
+		var blocks []string
+		if strings.Contains(blk, "[") {
+			// convert input to list of blocks
+			blk = strings.Replace(blk, "[", "", -1)
+			blk = strings.Replace(blk, "]", "", -1)
+			blk = strings.Replace(blk, "'", "", -1)
+			for _, v := range strings.Split(blk, ",") {
+				blocks = append(blocks, strings.Trim(v, " "))
+			}
+		} else {
+			blocks = append(blocks, blk)
+		}
 		var binds []string
-		genSQL, binds = TokenGenerator(block, 100, "block_token") // 100 is max for # of allowed datasets
+		genSQL, binds = TokenGenerator(blocks, 100, "block_token") // 100 is max for # of allowed datasets
 		for _, v := range binds {
 			args = append(args, v)
 		}
