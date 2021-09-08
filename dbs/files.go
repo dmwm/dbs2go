836fa06fe799a02281519bd04f3c626b1668d56e
Valentin Kuznetsov <vkuznet@gmail.com>
2021-09-08 14:38:54 -0400

    ignore some functions for gocyclo tests

--- dbs/files.go
+++ dbs/files.go
@@ -14,8 +14,8 @@ import (
 	"github.com/vkuznet/dbs2go/utils"
 )
 
-//gocyclo:ignore
 // Files DBS API
+//gocyclo:ignore
 func (a *API) Files() error {
 	var args []interface{}
 	var conds, lumis []string
