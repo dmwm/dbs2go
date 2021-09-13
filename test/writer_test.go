2118f4452efbf883156f866a3fdfffd3ef7ffc6b
Valentin Kuznetsov <vkuznet@gmail.com>
2021-09-13 09:22:02 -0400

    re-factor insertData API to accept db pointer

--- test/writer_test.go
+++ test/writer_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -19,134 +20,149 @@ import (
 
 // TestDBSWriter provides a test to DBS writer functionality
 func TestDBSWriter(t *testing.T) {
+	// initialize DB for testing
+	db := initDB(false)
+	var err error
+
 	api := "/primarydatasets"
 	hdlr := web.PrimaryDatasetsHandler
 	log.Println("insert primary dataset")
-	insertData(t, api, "data/primarydataset.json", "primary_ds_name", hdlr)
+	insertData(t, db, api, "data/primarydataset.json", "primary_ds_name", hdlr)
 	log.Println("re-insert primary dataset")
-	insertData(t, api, "data/primarydataset.json", "primary_ds_name", hdlr)
+	insertData(t, db, api, "data/primarydataset.json", "primary_ds_name", hdlr)
 
 	api = "/outputconfigs"
 	hdlr = web.OutputConfigsHandler
 	log.Println("insert output config")
-	insertData(t, api, "data/outputconfig.json", "output_module_label", hdlr)
+	insertData(t, db, api, "data/outputconfig.json", "output_module_label", hdlr)
 	log.Println("re-insert output config")
-	insertData(t, api, "data/outputconfig.json", "output_module_label", hdlr)
+	insertData(t, db, api, "data/outputconfig.json", "output_module_label", hdlr)
 
 	api = "/acquisitioneras"
 	hdlr = web.AcquisitionErasHandler
 	log.Println("insert acquisition era")
-	insertData(t, api, "data/acquisitionera.json", "acquisition_era_name", hdlr)
+	insertData(t, db, api, "data/acquisitionera.json", "acquisition_era_name", hdlr)
 	log.Println("re-insert acquisition era")
-	insertData(t, api, "data/acquisitionera.json", "acquisition_era_name", hdlr)
+	insertData(t, db, api, "data/acquisitionera.json", "acquisition_era_name", hdlr)
 
 	api = "/processingeras"
 	hdlr = web.ProcessingErasHandler
 	log.Println("insert processing era")
-	insertData(t, api, "data/processingera.json", "processing_era_name", hdlr)
+	insertData(t, db, api, "data/processingera.json", "processing_version", hdlr)
 	log.Println("re-insert processing era")
-	insertData(t, api, "data/processingera.json", "processing_era_name", hdlr)
+	insertData(t, db, api, "data/processingera.json", "processing_version", hdlr)
 
 	api = "/datatiers"
 	hdlr = web.DatatiersHandler
 	log.Println("insert data tier")
-	insertData(t, api, "data/datatier.json", "data_tier_name", hdlr)
+	insertData(t, db, api, "data/datatier.json", "data_tier_name", hdlr)
 	log.Println("re-insert data tier")
-	insertData(t, api, "data/datatier.json", "data_tier_name", hdlr)
+	insertData(t, db, api, "data/datatier.json", "data_tier_name", hdlr)
 
 	api = "/datasetaccesstypes"
 	hdlr = web.DatasetAccessTypesHandler
 	log.Println("insert dataset access type")
-	insertData(t, api, "data/datasetaccesstype.json", "dataset_access_type", hdlr)
+	insertData(t, db, api, "data/datasetaccesstype.json", "dataset_access_type", hdlr)
 	log.Println("re-insert dataset access type")
-	insertData(t, api, "data/datasetaccesstype.json", "dataset_access_type", hdlr)
+	insertData(t, db, api, "data/datasetaccesstype.json", "dataset_access_type", hdlr)
 
 	api = "/physicsgroups"
 	hdlr = web.PhysicsGroupsHandler
 	log.Println("insert physics group")
-	insertData(t, api, "data/physicsgroup.json", "physics_group_name", hdlr)
+	insertData(t, db, api, "data/physicsgroup.json", "physics_group_name", hdlr)
 	log.Println("re-insert physics group")
-	insertData(t, api, "data/physicsgroup.json", "physics_group_name", hdlr)
+	insertData(t, db, api, "data/physicsgroup.json", "physics_group_name", hdlr)
 
 	api = "/datasets"
 	hdlr = web.DatasetsHandler
 	log.Println("insert dataset")
-	insertData(t, api, "data/dataset.json", "dataset", hdlr)
+	insertData(t, db, api, "data/dataset.json", "dataset", hdlr)
 	log.Println("re-insert dataset")
-	insertData(t, api, "data/dataset.json", "dataset", hdlr)
+	insertData(t, db, api, "data/dataset.json", "dataset", hdlr)
 
 	api = "/blocks"
 	hdlr = web.BlocksHandler
 	log.Println("insert block")
-	insertData(t, api, "data/block.json", "block_name", hdlr)
+	insertData(t, db, api, "data/block.json", "block_name", hdlr)
 	log.Println("re-insert block")
-	insertData(t, api, "data/block.json", "block_name", hdlr)
+	insertData(t, db, api, "data/block.json", "block_name", hdlr)
 
 	// use datasetparent.json to insert dataset record used by datasetparent.json
 	api = "/datasets"
 	hdlr = web.DatasetsHandler
 	log.Println("insert dataset")
-	insertData(t, api, "data/datasetparent.json", "dataset_name", hdlr)
+	insertData(t, db, api, "data/datasetparent.json", "dataset", hdlr)
 	log.Println("re-insert dataset")
-	insertData(t, api, "data/datasetparent.json", "dataset_name", hdlr)
+	insertData(t, db, api, "data/datasetparent.json", "dataset", hdlr)
 
 	// use blockparent.json to insert block record used by fileparent.json
 	api = "/blocks"
 	hdlr = web.BlocksHandler
 	log.Println("insert block")
-	insertData(t, api, "data/blockparent.json", "block_name", hdlr)
+	insertData(t, db, api, "data/blockparent.json", "block_name", hdlr)
 	log.Println("re-insert block")
-	insertData(t, api, "data/blockparent.json", "block_name", hdlr)
+	insertData(t, db, api, "data/blockparent.json", "block_name", hdlr)
 
 	// use fileparent.json to insert file records which later will be used
 	// as file parents in subsequent call with files.json
 	api = "/files"
 	hdlr = web.FilesHandler
 	log.Println("insert file parents")
-	insertData(t, api, "data/fileparent.json", "logical_file_name", hdlr)
+	insertData(t, db, api, "data/fileparent.json", "", hdlr)
 	log.Println("re-insert file parents")
-	insertData(t, api, "data/fileparent.json", "logical_file_name", hdlr)
+	insertData(t, db, api, "data/fileparent.json", "", hdlr)
 
 	api = "/files"
 	hdlr = web.FilesHandler
 	log.Println("insert files")
-	insertData(t, api, "data/file.json", "", hdlr)
+	insertData(t, db, api, "data/file.json", "", hdlr)
 	log.Println("re-insert files")
-	insertData(t, api, "data/file.json", "", hdlr)
+	insertData(t, db, api, "data/file.json", "", hdlr)
 
 	api = "/bulkblocks"
 	hdlr = web.BulkBlocksHandler
 	log.Println("insert bulk block")
-	insertData(t, api, "data/bulkblock.json", "block_name", hdlr)
+	insertData(t, db, api, "data/bulkblock.json", "", hdlr)
 	log.Println("re-insert bulk block")
-	insertData(t, api, "data/bulkblock.json", "block_name", hdlr)
+	insertData(t, db, api, "data/bulkblock.json", "", hdlr)
+
+	t.Logf("finish DBS writer test")
+	err = db.Close()
+	if err != nil {
+		t.Error(err.Error())
+	}
 }
 
 // insertData provides a test to insert DBS data
-func insertData(t *testing.T, api, dataFile, attr string, hdlr func(http.ResponseWriter, *http.Request)) {
-	// initialize DB for testing
-	db := initDB(false)
-	defer db.Close()
-
+func insertData(t *testing.T, db *sql.DB, api, dataFile, attr string, hdlr func(http.ResponseWriter, *http.Request)) {
 	// setup HTTP request
 	var data []byte
 	var err error
 	var rr *httptest.ResponseRecorder
 	data, err = os.ReadFile(dataFile)
 	if err != nil {
-		t.Error(err.Error())
+		log.Printf("ERROR: unable to read %s error %v", dataFile, err.Error())
+		t.Fatal(err.Error())
 	}
 	var rec dbs.Record
 	err = json.Unmarshal(data, &rec)
 	if err != nil {
-		t.Errorf("unable to unmarshal received data '%s', error %v", string(data), err)
+		log.Printf("unable to unmarshal received data into dbs.Record, error %v, try next []dbs.Record", err)
+		// let's try to load list of records
+		var rrr []dbs.Record
+		err = json.Unmarshal(data, &rrr)
+		if err != nil {
+			t.Fatalf("ERROR: unable to unmarshal received data '%s', error %v", string(data), err)
+		}
+		log.Println("succeed to load record as []dbs.Record")
 	}
 	reader := bytes.NewReader(data)
 
 	// test writer POST DBS API
 	rr, err = respRecorder("POST", api, reader, hdlr)
 	if err != nil {
-		t.Error(err)
+		log.Printf("ERROR: unable to make POST HTTP request with api=%s, error %v", api, err)
+		t.Fatal(err)
 	}
 
 	log.Printf("writer api %s send data:\n%v", api, string(data))
@@ -156,23 +172,26 @@ func insertData(t *testing.T, api, dataFile, attr string, hdlr func(http.Respons
 	data = rr.Body.Bytes()
 	err = json.Unmarshal(data, &records)
 	if err != nil {
-		t.Errorf("unable to unmarshal received data '%s', error %v", string(data), err)
+		t.Fatalf("ERROR: unable to unmarshal received data '%s', error %v", string(data), err)
 	}
+
+	log.Printf("writer api %s returns %v, len(records)=%d", api, string(data), len(records))
 
 	// we should receive empty list
 	emptyList := "[]"
 	if len(records) != 0 && string(data) != emptyList {
-		t.Errorf("writer POST api %s, wrong output %v", api, string(data))
+		t.Fatalf("ERROR: writer POST api %s, wrong output %v", api, string(data))
 	}
 
 	// if no attribute is provided we'll skip GET API test
 	if attr == "" {
+		log.Println("skip get API call since no attr is provided")
 		return
 	}
 	// test reader GET DBS API
 	val, ok := rec[attr]
 	if !ok {
-		t.Error("unable to extract primary_ds_name from loaded record")
+		t.Fatalf("ERROR: unable to extract %s from loaded record", attr)
 	}
 	var value string
 	switch v := val.(type) {
@@ -184,22 +203,15 @@ func insertData(t *testing.T, api, dataFile, attr string, hdlr func(http.Respons
 	getApi := fmt.Sprintf("%s?%s=%s", api, attr, value)
 	rr, err = respRecorder("GET", getApi, reader, hdlr)
 	if err != nil {
-		t.Error(err)
+		log.Printf("ERROR: unable to place GET HTTP request with api=%s, error %v", getApi, err)
+		t.Fatal(err)
 	}
 
 	// unmarshal received records
 	data = rr.Body.Bytes()
 	err = json.Unmarshal(data, &records)
 	if err != nil {
-		t.Errorf("unable to unmarshal received data '%s', error %v", string(data), err)
+		t.Fatalf("ERROR: unable to unmarshal received data '%s', error %v", string(data), err)
 	}
 	log.Printf("reader api %s received data:\n%v", getApi, string(data))
-}
-
-// insertOutputConfig provides a test to insert output config data
-func insertOutputConfig(t *testing.T) {
-}
-
-// insertAcquisitionEra provides a test to insert acquisition era
-func insertAcquisitionEra(t *testing.T) {
 }
