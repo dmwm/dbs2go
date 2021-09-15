a1cdc8ee50d51337408b61e63333a477cad885fa
Valentin Kuznetsov <vkuznet@gmail.com>
2021-09-15 16:10:43 -0400

    Implement InsertFileParentsBlockTxt

--- dbs/fileparents.go
+++ dbs/fileparents.go
@@ -83,12 +83,25 @@ func (r *FileParents) Insert(tx *sql.Tx) error {
 		log.Println("unable to validate record", err)
 		return err
 	}
+
+	// check if our data already exist in DB
+	var vals []interface{}
+	vals = append(vals, r.THIS_FILE_ID)
+	vals = append(vals, r.PARENT_FILE_ID)
+	args := []string{"this_file_id", "parent_file_id"}
+	if IfExistMulti(tx, "FILE_PARENTS", "this_file_id", args, vals...) {
+		return nil
+	}
+
 	// get SQL statement from static area
 	stm := getSQL("insert_fileparents")
 	if utils.VERBOSE > 0 {
 		log.Printf("Insert FileParents\n%s\n%+v", stm, r)
 	}
 	_, err = tx.Exec(stm, r.THIS_FILE_ID, r.PARENT_FILE_ID)
+	if err != nil {
+		log.Println("unable to execute", stm, "error", err)
+	}
 	return err
 }
 
@@ -135,6 +148,12 @@ type FileParentRecord struct {
 	ParentLogicalFileName string `json:"parent_logical_file_name"`
 }
 
+// FileParentBlockRecord represents file parent DBS record
+type FileParentBlockRecord struct {
+	BlockName         string    `json:"block_name"`
+	ChildParentIDList [][]int64 `json:"child_parent_id_list"`
+}
+
 // InsertFileParents DBS API
 func (a *API) InsertFileParents() error {
 	// start transaction
@@ -146,8 +165,12 @@ func (a *API) InsertFileParents() error {
 	defer tx.Rollback()
 	err = a.InsertFileParentsTxt(tx)
 	if err != nil {
-		log.Println("unable to insert file parents", err)
-		return err
+		// let's try to insert via FileParentBlock record
+		err = a.InsertFileParentsBlockTxt(tx)
+		if err != nil {
+			log.Println("unable to insert file parents", err)
+			return err
+		}
 	}
 
 	// commit transaction
@@ -164,17 +187,9 @@ func (a *API) InsertFileParents() error {
 
 // InsertFileParentsTxt DBS API
 func (a *API) InsertFileParentsTxt(tx *sql.Tx) error {
-	// TODO: implement the following logic
-	// /Users/vk/CMS/DMWM/GIT/DBS/Server/Python/src/dbs/business/DBSFile.py
-	/*
-	   input block_name: is a child block name.
-	   input chils_parent_id_list: is a list of file id of child, parent  pair: [[cid1, pid1],[cid2,pid2],[cid3,pid3],...]
-	   The requirement for this API is
-	   1. All the child files belong to the block.
-	   2. All the child-parent pairs are not already in DBS.
-	   3. The dataset parentage is already in DBS.
-	*/
-	// read given input
+	// implement the following logic
+	// /Users/vk/CMS/DMWM/GIT/DBS/Server/Python/src/dbs/dao/Oracle/FileParent/Insert.py
+
 	data, err := io.ReadAll(a.Reader)
 	if err != nil {
 		log.Println("fail to read data", err)
@@ -190,7 +205,14 @@ func (a *API) InsertFileParentsTxt(tx *sql.Tx) error {
 			log.Println("fail to decode data", err)
 			return err
 		}
-		records = append(records, rrr)
+		if rrr.LogicalFileName != "" {
+			records = append(records, rrr)
+		}
+	}
+	if len(records) == 0 {
+		msg := "fail to decode input request as FileParentRecord(s)"
+		log.Println(msg)
+		return errors.New(msg)
 	}
 	for _, rec := range records {
 		if utils.VERBOSE > 0 {
@@ -216,6 +238,91 @@ func (a *API) InsertFileParentsTxt(tx *sql.Tx) error {
 		}
 		err = rrr.Insert(tx)
 		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// InsertFileParentsBlockTxt DBS API
+func (a *API) InsertFileParentsBlockTxt(tx *sql.Tx) error {
+	// TODO: implement the following logic
+	// /Users/vk/CMS/DMWM/GIT/DBS/Server/Python/src/dbs/dao/Oracle/FileParent/Insert2.py
+	/*
+	   input block_name: is a child block name.
+	   input chils_parent_id_list: is a list of file id of child, parent  pair: [[cid1, pid1],[cid2,pid2],[cid3,pid3],...]
+	   The requirement for this API is
+	   1. All the child files belong to the block.
+	   2. All the child-parent pairs are not already in DBS.
+	   3. The dataset parentage is already in DBS.
+	*/
+
+	var args []interface{}
+	var conds []string
+
+	// read given input
+	data, err := io.ReadAll(a.Reader)
+	if err != nil {
+		log.Println("fail to read data", err)
+		return err
+	}
+	var rec FileParentBlockRecord
+	err = json.Unmarshal(data, &rec)
+	if err != nil {
+		log.Println("fail to decode data as FileParentBlockRecord", err)
+		return err
+	}
+	if utils.VERBOSE > 0 {
+		log.Printf("Insert FileParentsBlock record %+v", rec)
+	}
+
+	conds, args = AddParam("block_name", "B.BLOCK_NAME", a.Params, conds, args)
+	stm := getSQL("fileparents_block")
+	stm = WhereClause(stm, conds)
+	if err != nil {
+		return err
+	}
+	// get file ids associated with given block name
+	rows, err := tx.Query(stm, args...)
+	if err != nil {
+		msg := fmt.Sprintf("unable to query statement:\n%v\nerror=%v", stm, err)
+		return errors.New(msg)
+	}
+	defer rows.Close()
+	var bfids []int64
+	for rows.Next() {
+		var fid int64
+		if err := rows.Scan(&fid); err != nil {
+			return err
+		}
+		bfids = append(bfids, fid)
+	}
+	// check that out file ids from block are the same as from child_parent_id_list
+	var fids []int64
+	if vals, ok := a.Params["child_parent_id_list"]; ok {
+		v := vals.([]int64)
+		fids = append(fids, v[0])
+	}
+	if len(fids) != len(bfids) {
+		log.Println("block fids != file ids")
+		log.Println("block ids", bfids)
+		log.Println("file  ids", fids)
+		msg := fmt.Sprintf("not all files present in block")
+		return errors.New(msg)
+	}
+	// now we can loop over provided list and insert file parents
+	if vals, ok := a.Params["child_parent_id_list"]; ok {
+		v := vals.([]int64)
+		var r FileParents
+		r.THIS_FILE_ID = v[0]
+		r.PARENT_FILE_ID = v[1]
+		err = r.Validate()
+		if err != nil {
+			return err
+		}
+		err = r.Insert(tx)
+		if err != nil {
+			log.Println("unable to insert FileParents record, error", err)
 			return err
 		}
 	}
