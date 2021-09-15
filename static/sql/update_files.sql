c5403be45041f46e7afe29150d7d3c42d6a10e3e
Valentin Kuznetsov <vkuznet@gmail.com>
2021-09-15 08:02:56 -0400

    update sql statement

--- static/sql/update_files.sql
+++ static/sql/update_files.sql
@@ -1,4 +1,16 @@
+{{.TokenGenerator}}
+{{if .SQLite}}
+UPDATE {{.Owner}}.FILES
+{{else}}
 UPDATE {{.Owner}}.FILES F
+{{end}}
     SET LAST_MODIFIED_BY=:myuser,
         LAST_MODIFICATION_DATE=:mydate,
         IS_FILE_VALID = :is_file_valid
+{{if .Dataset}}
+F.dataset_id in (
+    SELECT D.dataset_id FROM {{.Owner}}.DATASETS D
+    INNER JOIN {{.Owner}}.FILES F2 ON F2.dataset_id = D.dataset_id
+    WHERE D.dataset=:dataset
+)
+{{end}}
