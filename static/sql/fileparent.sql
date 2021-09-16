d6d1775618e0de6613ea490028a1ff2366d0c7f0
Valentin Kuznetsov <vkuznet@gmail.com>
2021-09-16 13:10:22 -0400

    change this_logical_file_name to logical_file_name

--- static/sql/fileparent.sql
+++ static/sql/fileparent.sql
@@ -1,4 +1,4 @@
-SELECT F.LOGICAL_FILE_NAME this_logical_file_name, PF.LOGICAL_FILE_NAME parent_logical_file_name,
+SELECT F.LOGICAL_FILE_NAME logical_file_name, PF.LOGICAL_FILE_NAME parent_logical_file_name,
         PF.FILE_ID parent_file_id
         FROM {{.Owner}}.FILES PF
         JOIN {{.Owner}}.FILE_PARENTS FP ON FP.PARENT_FILE_ID = PF.FILE_ID
