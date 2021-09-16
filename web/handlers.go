5797cf63fbc5ac86b5bd7409a607e9630f8ade2a
Valentin Kuznetsov <vkuznet@gmail.com>
2021-09-16 08:33:09 -0400

    reverse parsePayload for fileparents

--- web/handlers.go
+++ web/handlers.go
@@ -336,7 +336,7 @@ func DBSPostHandler(w http.ResponseWriter, r *http.Request, a string) {
 		CreateBy:  cby,
 		Api:       a,
 	}
-	if a == "fileArray" || a == "datasetlist" || a == "fileparentsbylumi" || a == "filelumis" || a == "blockparents" || a == "fileparents" {
+	if a == "fileArray" || a == "datasetlist" || a == "fileparentsbylumi" || a == "filelumis" || a == "blockparents" {
 		params, err = parsePayload(r)
 		if err != nil {
 			responseMsg(w, r, fmt.Sprintf("%v", err), a, http.StatusInternalServerError)
