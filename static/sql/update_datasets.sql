23c673c58472ffceca14b85d511023e783e4ce97
Valentin Kuznetsov <vkuznet@gmail.com>
2021-10-29 14:17:34 -0400

    fix typo in SQL statement

--- static/sql/update_datasets.sql
+++ static/sql/update_datasets.sql
@@ -1,6 +1,6 @@
 UPDATE {{.Owner}}.DATASETS
     SET LAST_MODIFIED_BY=:myuser,
         LAST_MODIFICATION_DATE=:mydate,
-        DATASET_ACCESS_TYPE = :dataset_access_type,
+        DATASET_ACCESS_TYPE_ID = :dataset_access_type_id,
         IS_DATASET_VALID = :is_dataset_valid
     WHERE DATASET = :dataset
