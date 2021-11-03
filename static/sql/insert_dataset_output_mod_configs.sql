2527686ca9bf99e7e5ebd7598d45db1e111f1d0a
Valentin Kuznetsov <vkuznet@gmail.com>
2021-11-03 08:55:37 -0400

    fix column name

--- static/sql/insert_dataset_output_mod_configs.sql
+++ static/sql/insert_dataset_output_mod_configs.sql
@@ -1,4 +1,4 @@
 INSERT INTO {{.Owner}}.DATASET_OUTPUT_MOD_CONFIGS
-    (ds_output_config_id,dataset_id,output_mod_config_id)
+    (ds_output_mod_conf_id,dataset_id,output_mod_config_id)
     VALUES
-    (:ds_output_config_id,:dataset_id,:output_mod_config_id)
+    (:ds_output_mod_conf_id,:dataset_id,:output_mod_config_id)
