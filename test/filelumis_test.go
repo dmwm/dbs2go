edbd6b63a40c15b339580ed8d956316027adec2f
Valentin Kuznetsov <vkuznet@gmail.com>
2021-11-16 10:06:18 -0500

    Add unit test for filelumis injection loop algorithm

---
+++ test/filelumis_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+var totalRequests uint64
+
+// helper function to immitate chunk insertion
+func insertChunk(wg *sync.WaitGroup, idx, limit int) {
+	defer wg.Done()
+	var args []interface{}
+	stm := "INSERT ALL"
+	for i := idx; i < limit; i++ {
+		into := `INTO ORA$PTT_TEMP_FILE_PARENTS (THIS_FILE_ID, PARENT_FILE_ID) VALUES (:fval, :pval)`
+		stm = fmt.Sprintf("%s\n%s", stm, into)
+		args = append(args, i)
+		args = append(args, i)
+		atomic.AddUint64(&totalRequests, 1)
+		//         s := fmt.Sprintf("%d", i)
+		//         if len(s) == 1 {
+		//             s = fmt.Sprintf("000%s", s)
+		//         } else if len(s) == 2 {
+		//             s = fmt.Sprintf("00%s", s)
+		//         } else if len(s) == 3 {
+		//             s = fmt.Sprintf("0%s", s)
+		//         }
+		//         fmt.Println("idx", s)
+	}
+	stm = fmt.Sprintf("%s\nSELECT * FROM dual", stm)
+	// here stm represents final statement
+	// instead of doing tx.Exec we'll sleep
+	time.Sleep(10 * time.Millisecond)
+}
+
+// TestFileLumisInjectionLoop API
+func TestFileLumisInjectionLoop(t *testing.T) {
+	nrec := 3000
+	maxSize := 110
+	chunkSize := 25
+	// inject some data into temp table
+	if maxSize > nrec {
+		maxSize = nrec
+	}
+	for k := 0; k < nrec; k = k + maxSize {
+		t0 := time.Now()
+		var wg sync.WaitGroup
+		ngoroutines := 0
+		for i := k; i < k+maxSize; i = i + chunkSize {
+			wg.Add(1)
+			size := i + chunkSize
+			if size > (k + maxSize) {
+				size = k + maxSize
+			}
+			if size > nrec {
+				size = nrec
+			}
+			//             log.Printf("k=%d i=%d size=%d max=%d nrec=%d", k, i, size, maxSize, nrec)
+			go insertChunk(&wg, i, size)
+			ngoroutines += 1
+		}
+		limit := k + maxSize
+		if limit > nrec {
+			limit = nrec
+		}
+		log.Printf("process %d goroutines, step %d-%d, elapsed time %v", ngoroutines, k, limit, time.Since(t0))
+		wg.Wait()
+	}
+	if int64(totalRequests) != int64(nrec) {
+		t.Errorf("wrong number of processed events nrec=%d tot=%d", nrec, totalRequests)
+	}
+}
