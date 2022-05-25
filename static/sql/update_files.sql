444f7f39e4f915ae412c047eeb253481a895607a
Valentin Kuznetsov <vkuznet@gmail.com>
2022-05-25 08:57:42 -0400

    fix query

--- static/sql/update_files.sql
+++ static/sql/update_files.sql
@@ -9,9 +9,9 @@ UPDATE {{.Owner}}.FILES F
         IS_FILE_VALID = :is_file_valid
 {{if .Dataset}}
 {{if .SQLite}}
-AND dataset_id in (
+WHERE dataset_id in (
 {{else}}
-AND F.dataset_id in (
+WHERE F.dataset_id in (
 {{end}}
     SELECT D.dataset_id FROM {{.Owner}}.DATASETS D
     INNER JOIN {{.Owner}}.FILES F2 ON F2.dataset_id = D.dataset_id
