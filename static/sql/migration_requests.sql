ffc79e3fba4689062af5a9fc37ac9380ba988cbd
Valentin Kuznetsov <vkuznet@gmail.com>
2022-07-20 09:40:53 -0400

    Fix query to allow block_name clause

--- static/sql/migration_requests.sql
+++ static/sql/migration_requests.sql
@@ -3,6 +3,9 @@ SELECT MR.MIGRATION_REQUEST_ID, MR.MIGRATION_URL,
        MR.CREATE_BY, MR.CREATION_DATE,
        MR.LAST_MODIFIED_BY, MR.LAST_MODIFICATION_DATE, MR.RETRY_COUNT
 FROM {{.Owner}}.MIGRATION_REQUESTS MR
+{{if .Blocks}}
+JOIN {{.Owner}}.MIGRATION_BLOCKS MB ON MB.MIGRATION_REQUEST_ID=MR.MIGRATION_REQUEST_ID
+{{end}}
 
 {{if .Oldest}}
 WHERE MR.MIGRATION_STATUS=0
@@ -12,5 +15,4 @@ or (MR.migration_status=3 and MR.retry_count=2 and MR.last_modification_date <= 
 --or (MR.migration_status=0 and MR.retry_count=0 and MR.last_modification_date <= {{.PendingDate}})
 --or (MR.migration_status=1 and MR.retry_count=0 and MR.last_modification_date <= {{.ProgressDate}})
 or MR.MIGRATION_STATUS=1
-ORDER BY MR.creation_date
 {{end}}
