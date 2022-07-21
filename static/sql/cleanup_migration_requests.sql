3f4a3730ff6171ea9b6fd31fbba0a5ab5db9200e
Valentin Kuznetsov <vkuznet@gmail.com>
2022-07-21 13:35:34 -0400

    fix delete query from migration requsets table

--- static/sql/cleanup_migration_requests.sql
+++ static/sql/cleanup_migration_requests.sql
@@ -1,4 +1,4 @@
-DELETE FROM {{.Owner}}.MIGRATION_REQUESTS MR
-WHERE MR.LAST_MODIFICATION_DATE <= {{.Value}}
-or (MR.migration_status=3 and MR.retry_count=3 and MR.last_modification_date <= {{.FailDate}})
-or (MR.migration_status=9 and MR.retry_count=3 and MR.last_modification_date <= {{.FailDate}})
+DELETE FROM {{.Owner}}.MIGRATION_REQUESTS
+WHERE LAST_MODIFICATION_DATE <= {{.Value}}
+or (migration_status=3 and retry_count=3 and last_modification_date <= {{.FailDate}})
+or (migration_status=9 and retry_count=3 and last_modification_date <= {{.FailDate}})
