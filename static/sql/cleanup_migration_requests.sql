46f5045ef12104e79d36df41625023e5a9f5d487
Valentin Kuznetsov <vkuznet@gmail.com>
2022-08-25 12:10:14 -0400

    Adjust query

--- static/sql/cleanup_migration_requests.sql
+++ static/sql/cleanup_migration_requests.sql
@@ -1,4 +1,4 @@
 DELETE FROM {{.Owner}}.MIGRATION_REQUESTS
 WHERE LAST_MODIFICATION_DATE <= {{.Value}}
-or (migration_status=3 and retry_count=3 and last_modification_date <= {{.FailDate}})
-or (migration_status=9 and retry_count=3 and last_modification_date <= {{.FailDate}})
+or (migration_status=3 and retry_count>2 and last_modification_date <= {{.FailDate}})
+or (migration_status=9 and retry_count>2 and last_modification_date <= {{.FailDate}})
